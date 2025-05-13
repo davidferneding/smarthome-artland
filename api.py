e80a34c1dac6f85ba69626d262cb35d3abf1fa93
Benjamin <bennysmail@t-online.de>
2025-05-13 17:50:29 +0200

    -Enum zum einordnen von Geräten -Get-Endpunkt auss den"Vorschlägen"(wahrscheinlich nicht endgültig...)

--- api.py
+++ api.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI , HTTPException
-
+from enum import Enum
 from fastapi.middleware.cors import CORSMiddleware
 import uuid
 
 devicelist={}
 
 class Device:
-    def __init__(self, id, name):
+    def __init__(self, id, name, type):
         self.id = id
         self.name = name
+        self.type = type
         
+class DeviceType(Enum):
+    lampe = "lampe"
+    clickbot = "clickbot"
+    andere = "andere"
 
 app = FastAPI()
 app.add_middleware(
@@ -21,19 +26,16 @@ app.add_middleware(
 )
 
 @app.post("/add-device")
-def adddevice(name):
+def adddevice(name, type: DeviceType):
     id = str(uuid.uuid4())
-    device = Device(id,name)
+    device = Device(id, name, type)
     devicelist.setdefault(id,device)
-    return id
+    return {
+        "id": id,
+        "name": name,
+        "type": type
+    }
 
-
-
-
-
- 
-
-
-
-
-
+@app.get("/get-devices")
+def getdevices():
+    return {"devices": [device.__dict__ for device in devicelist.values()]}
