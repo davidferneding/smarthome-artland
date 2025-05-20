d56811866e2e7355607100fe09f4767539515fc5
Benny <bennysmail@t-online.de>
2025-05-20 14:27:32 +0200

    add change status

--- api.py
+++ api.py
@@ -5,16 +5,24 @@ import uuid
 
 devicelist={}
 
-class Device:
-    def __init__(self, id, name, type):
-        self.id = id
-        self.name = name
-        self.type = type
-        
 class DeviceType(Enum):
     lampe = "lampe"
     clickbot = "clickbot"
     andere = "andere"
+
+class DeviceStatus(Enum):
+    on = "on"
+    off = "off"
+    offline = "offline"
+
+
+class Device:
+    def __init__(self, id, name, type, status):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.status = status
+        
 
 app = FastAPI()
 app.add_middleware(
@@ -28,14 +36,17 @@ app.add_middleware(
 @app.post("/add-device")
 def adddevice(name, type: DeviceType):
     id = str(uuid.uuid4())
-    device = Device(id, name, type)
+    device = Device(id, name, type, DeviceStatus.on)
     devicelist.setdefault(id,device)
-    return {
-        "id": id,
-        "name": name,
-        "type": type
-    }
+    return device
 
 @app.get("/get-devices")
 def getdevices():
     return {"devices": [device.__dict__ for device in devicelist.values()]}
+
+@app.post("/change-status")
+def changeStatus(id, targetstatus: DeviceStatus):
+    device = devicelist[id]
+    device.status = targetstatus
+    return device
+
